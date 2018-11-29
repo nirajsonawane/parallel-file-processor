599dd712bed8b5f5423f581dd22a7d1d0a499241
nirajsonawane <niraj.sonawane@gmail.com>
2018-11-29 09:53:18 -0800

    Test commit from new laptop

--- src/main/java/com/niraj/Application.java
+++ src/main/java/com/niraj/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);   
+        SpringApplication.run(Application.class, args); 
+        
     }
 }
